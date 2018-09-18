36d0cd558c17a4115fe0eef5a402977bba14284a
Andreas H. From <andro.from@gmail.com>
2018-09-18 13:31:18 +0200

    merge

--- src/mas/Runner.java
+++ src/mas/Runner.java
@@ -6,11 +6,13 @@ import massim.Server;
 public class Runner {
 
 	public static void main(String[] args) throws JasonException {
+		/*
 		new Thread(new Runnable() {
 			public void run() {
 				Server.main(new String[] { "-conf", "conf/SampleConfig.json", "--monitor" });
 			}
 		}).start();
+		*/
 
 		RunCentralisedMAS.main(new String[] { "src/mas/multiagent_jason.mas2j" });
 	}
