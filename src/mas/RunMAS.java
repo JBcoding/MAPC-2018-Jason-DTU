3fbfcc963e0b1e7ddb4cc8d24f3005830005f846
Henney <Henney>
2018-09-24 10:54:12 +0200

    Hopefully fix going to location that is to far on one charge

--- src/mas/RunMAS.java
+++ src/mas/RunMAS.java
@@ -2,10 +2,12 @@ package mas;
 
 import jason.JasonException;
 import jason.infra.centralised.RunCentralisedMAS;
+import massim.scenario.city.data.Location;
 
 public class RunMAS {
 
 	public static void main(String[] args) throws JasonException {
+		Location.setProximity(5);
 		RunCentralisedMAS.main(new String[] { "src/mas/multiagent_jason.mas2j" });
 	}
 }
