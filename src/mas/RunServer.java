ef9d961d78025f939108db8377939de0b12e9d51
Henney <Henney>
2018-09-24 08:58:51 +0200

    Push comp like config

--- src/mas/RunServer.java
+++ src/mas/RunServer.java
@@ -6,6 +6,6 @@ public class RunServer {
 
 	public static void main(String[] args)
 	{
-		Server.main(new String[] { "-conf", "conf/SampleConfig.json", "--monitor" });
+		Server.main(new String[] { "-conf", "conf/CompLikeConfig.json", "--monitor" });
 	}
 }
